f06b0978c6e26f3cca17cff2e44f8da7dbd6be7f
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-05 17:36:52 +0530

    Update

--- Gtts.py
+++ Gtts.py
@@ -5,10 +5,10 @@ import os
 f = Figlet(font = 'slant')
 print (f.renderText('Text_to_speech'))
 #('Text_to_speech') program
-myText = 'hello my name is Ro706 nice to meet u and this is my first text to speech conversion program by using python.'
+myText = input('enter:')
 language = 'en'
 output = gTTS(text=myText,lang=language)
 output.save('gtts.mp3')
-os.system("mpv /data/data/com.termux/files/home/project")
+os.system("mpv /data/data/com.termux/files/home")
 #window user
 #os.system('start gtts.mp3')
