d7498f68f0a88e3e77954ad9d62b4cb728927fd7
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-07 18:24:36 +0530

    Update

--- Gtts.py
+++ Gtts.py
@@ -1,6 +1,18 @@
 from gtts import gTTS
 from pyfiglet import Figlet
 import os
+
+hour = int(datetime.datetime.now().hour)
+if hour >= 0 and hour < 12:
+    a = 'Good morning Rohit'
+elif hour >= 12 and hour <= 16:
+    a = 'Good afternoon Rohit'
+else:
+    a = 'Good evening Rohit'
+output1=gTTS(text=myText,lang=language)
+output1.save('wishme.mp3')
+os.system("mpv /data/data/com.termux/files/home/wishme.mp3")
+
 #banner
 f = Figlet(font = 'slant')
 print (f.renderText('Text_to_speech'))
@@ -9,6 +21,6 @@ myText = input('enter:')
 language = 'en'
 output = gTTS(text=myText,lang=language)
 output.save('gtts.mp3')
-os.system("mpv /data/data/com.termux/files/home/Text_to_speech")
+os.system("mpv /data/data/com.termux/files/home/gtts.mp3")
 #window user
 #os.system('start gtts.mp3')
